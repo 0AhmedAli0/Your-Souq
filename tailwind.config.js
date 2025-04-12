3b963fdd43dacfac4dee544c028e2179a73d8042
Ahmed <ahmed.ali050607@gmail.com>
2025-04-12 19:49:23 +0200

    Angular setup

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+ /** @type {import('tailwindcss').Config} */
+ //التي سنستخدمها tailWind class هذا الملف يخبرنا بالملفات التي يجب ان تبحث عنها عندما يتعلق الامر ب
+ export default {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}

42e941d0191cf67a8032ef35900a4310fa940628
Ahmed <ahmed.ali050607@gmail.com>
2025-04-13 20:36:21 +0200

    Header component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './layout/header/header.component';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [/*RouterOutlet*/],
+  imports: [/*RouterOutlet, */ HeaderComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
+  baseUrl = 'https://localhost:7196/api/';
+  // constructor(private _HttpClient: HttpClient) {} // old inject
+  private http = inject(HttpClient);
+
   title = 'Client';
 }

132effc34d4ab330bfc51eab3b393ca5f6d18735
Ahmed <ahmed.ali050607@gmail.com>
2025-04-14 19:50:41 +0200

    product page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,9 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './layout/header/header.component';
 import { HttpClient } from '@angular/common/http';
+import { product } from './shared/models/product';
+import { Pagination } from './shared/models/pagination';
 
 @Component({
   selector: 'app-root',
@@ -10,10 +12,21 @@ import { HttpClient } from '@angular/common/http';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   baseUrl = 'https://localhost:7196/api/';
   // constructor(private _HttpClient: HttpClient) {} // old inject
   private http = inject(HttpClient);
+  products: product[] = [];
 
   title = 'Client';
+  ngOnInit(): void {
+    //recive observable and cast it into products array using map function >> cast بمعني وضع او صب
+    this.http.get<Pagination<product>>(this.baseUrl + 'Products').subscribe({
+      next: (response) => (this.products = response.data),
+      error: (err) => console.log(err),
+      complete() {
+        console.log('Complete');
+      },
+    });
+  }
 }
---
+++ src/app/shared/models/product.ts
@@ -0,0 +1,19 @@
+export type product = {
+  id: number;
+  name: string;
+  description: string;
+  price: number;
+  pictureUrl: string;
+  ProductBrand: string;
+  ProductType: string;
+};
+// export type product = {
+//   id: number;
+//   name: string;
+//   description: string;
+//   price: number;
+//   pictureUrl: string;
+//   type: string;
+//   brand: string;
+//   quantityInStock: number;
+// };

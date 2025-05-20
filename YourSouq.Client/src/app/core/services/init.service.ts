2ac67119b665d3bbddd5a6c280a5e8289ba0109c
Ahmed <ahmed.ali050607@gmail.com>
2025-05-20 20:28:01 +0300

    Adding Login page

--- YourSouq.Client/src/app/core/services/init.service.ts
+++ YourSouq.Client/src/app/core/services/init.service.ts
@@ -1,12 +1,14 @@
 import { inject, Injectable } from '@angular/core';
 import { CartService } from './cart.service';
-import { of } from 'rxjs';
+import { forkJoin, of } from 'rxjs';
+import { AccountService } from './account.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class InitService {
   private cartService = inject(CartService);
+  private accountService = inject(AccountService);
 
   // This service is used to initialize the application
   //app initializer will wait for this service to finish before loading the app so that this function should return an observable
@@ -14,6 +16,10 @@ export class InitService {
     const cartId = localStorage.getItem('cart_id');
     const cart$ = cartId ? this.cartService.getCart(cartId) : of(null); //use of function from rxjs to return an observable
 
-    return cart$;
+    return forkJoin({
+      //forkJoin is used to combine multiple observables into one observable and wait for all of them to finish
+      cart: cart$,
+      user: this.accountService.getUserInfo(),
+    });
   }
 }
